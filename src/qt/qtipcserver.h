bab8fc9860951375255a53516d788d2493b15ace
BitCashCredit <30867258+BitCashCredit@users.noreply.github.com>
2017-08-12 07:08:56 +0530

    jolo.kell@gmx.com

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BitCashCredit-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BitCashCreditURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
